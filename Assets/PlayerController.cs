c2401b794eaefbab06980f83c166a2605d9c446c
Wolfgang Brabänder <40390115+CodingWaffel@users.noreply.github.com>
2021-10-06 21:34:20 +0200

    pre experimental push

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,21 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _speed = 10f;
     Rigidbody _rb;
+    Camera _playerCam;
 
     private void Awake() {
         this._rb = GetComponent<Rigidbody>();
+        
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         // this._rb.MovePosition(this._rb.position + (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * Time.fixedDeltaTime * this._speed));
-        this._rb.velocity = (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * Time.fixedDeltaTime * this._speed);
+        // this._rb.velocity = (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * Time.fixedDeltaTime * this._speed);
+        this._rb.velocity = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized * Time.fixedDeltaTime * this._speed;
+        this.transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
         
     }
 }
